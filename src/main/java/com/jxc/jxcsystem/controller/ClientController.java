a421a00b53c0c2f962ae27cafee126ec8df6b32e
Administrator <2475124724@qq.com>
2019-07-31 17:50:09 +0800

    0731客户crud

--- src/main/java/com/jxc/jxcsystem/controller/ClientController.java
+++ src/main/java/com/jxc/jxcsystem/controller/ClientController.java
@@ -2,6 +2,7 @@ package com.jxc.jxcsystem.controller;
 
 import com.jxc.jxcsystem.pojo.Client;
 import com.jxc.jxcsystem.service.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,7 +12,7 @@ import java.util.List;
 
 @Controller
 public class ClientController {
-    @Resource
+    @Autowired
     private ClientService clientService;
 
     @RequestMapping("/selectClient")
